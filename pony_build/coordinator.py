19543df0dd9aba3ce3935159114114b8054e2d34
Titus Brown <t@titus-browns-macbook.local>
2009-07-30 11:35:47 -0400

    extended quixote interface

--- pony_build/coordinator.py
+++ pony_build/coordinator.py
@@ -6,6 +6,7 @@ pony_build.server.get_coordinator().
 """
 
 import time
+import sets
 
 class PonyBuildCoordinator(object):
     def __init__(self, db=None):
@@ -73,6 +74,12 @@ class PonyBuildCoordinator(object):
             return x[-1]
         return None
 
+    def get_all_results_for_package(self, package):
+        l = self._packages.get(package, [])
+        if l:
+            return [ self.results_list[n] for n in l ]
+        return []
+
     def get_all_archs(self):
         k = self._archs.keys()
         k.sort()
@@ -106,4 +113,47 @@ class PonyBuildCoordinator(object):
                     d[arch] = (receipt, client_info, results)
 
         return d
+
+    def get_unique_tagset_for_package(self, package,
+                                      no_host=False, no_arch=False):
+        """
+        Get the 'unique' set of latest results for the given package,
+        based on tags, host, and architecture.  'no_host' says to
+        collapse multiple hosts, 'no_arch' says to ignore multiple
+        archs.
+
+        Returns a dictionary of (receipt, client_info, results_list)
+        tuples indexed by the set of keys used for 'uniqueness',
+        i.e. an ImmutableSet of the tags + host + arch.  For display
+        purposes, anything beginning with a '__' should be filtered
+        out of the keys.
         
+        """
+        result_indices = self._packages.get(package)
+        if not result_indices:
+            return {}
+
+        d = {}
+        for n in result_indices:
+            receipt, client_info, results_list = self.results_list[n]
+            arch = client_info['arch']
+            host = client_info['host']
+
+            tags = list(client_info['tags'])
+            if not no_arch:
+                tags.append('__' + arch)
+            if not no_host:
+                tags.append('__' + host)
+                
+            key = sets.ImmutableSet(tags)
+
+            # check if already stored
+            if key in d:
+                receipt2, _, _ = d[key]
+                # store the more recent one...
+                if receipt['time'] > receipt2['time']:
+                    d[key] = receipt, client_info, results_list
+            else:
+                d[key] = receipt, client_info, results_list
+
+        return d
--- pony_build/qx_web.py
+++ pony_build/qx_web.py
@@ -64,13 +64,13 @@ def create_publisher(coordinator):
 ###
 
 class PackageInfo(Directory):
-    _q_exports = [ '' ]
+    _q_exports = [ '', 'show_latest', 'show_all' ]
     
     def __init__(self, coord, package):
         self.coord = coord
         self.package = package
         
-    def _q_index(self):
+    def show_latest(self):
         package = self.package
         d = self.coord.get_latest_arch_result_for_package(package)
 
@@ -98,8 +98,9 @@ class PackageInfo(Directory):
         arch_list = [ k for (k, v) in it ]
 
         html = """
+<title>Build summary for '{{ packge }}'</title>
 <h2>Package '{{ package }}'</h2>
-Build information:<p>
+Build summary:<p>
 {% if d %}
    <table border='1'>
       <tr><th>Architecture</th><th>Status</th><th>last report</th></tr>
@@ -111,6 +112,122 @@ Build information:<p>
       </tr>
    {% endfor %}
    </table>
+   <p>
+   <a href='./'>show default report</a>
+   <p>
+   <a href='./show_all'>show all results</a>
+{% else %}
+ No results for this package!
+{% endif %}
+"""
+
+        return Template(html).render(locals())
+
+    def _q_index(self):
+        package = self.package
+        d = self.coord.get_unique_tagset_for_package(package)
+
+        def calc_status(tagset):
+            _, client_info, _ = d[tagset]
+            status = client_info['success']
+            if status:
+                return "<font color='green'>SUCCESS</font>"
+            else:
+                return "<font color='red'>FAILURE</font>"
+
+        def calc_time(tagset):
+            receipt, _, _ = d[tagset]
+            t = receipt['time']
+            return format_timestamp(t)
+
+        def get_host(tagset):
+            return d[tagset][1]['host']
+
+        def get_arch(tagset):
+            return d[tagset][1]['arch']
+
+        def nicetagset(tagset):
+            tagset = sorted([ x for x in list(tagset) if not x.startswith('__')])
+            return ", ".join(tagset)
+
+        def sort_by_timestamp(a, b):
+            ta = a[1][0]['time']
+            tb = b[1][0]['time']
+            return -cmp(ta, tb)
+
+        it = d.items()
+        it.sort(sort_by_timestamp)
+        tagset_list = [ k for (k, v) in it ]
+
+        html = """
+<title>Build summary for `{{ package }}`</title>
+<h2>Package '{{ package }}'</h2>
+Build summary:<p>
+{% if d %}
+   <table border='1'>
+      <tr><th>Tags</th><th>Host</th><th>Arch</th><th>Status</th><th>last report</th></tr>
+   {% for tagset in tagset_list %}
+      <tr>
+        <td>{{ nicetagset(tagset) }}</td>
+        <td>{{ get_host(tagset)}}</td>
+        <td>{{ get_arch(tagset)}}</td>
+        <td>{{ calc_status(tagset) }}</td>
+        <td>{{ calc_time(tagset) }}</td>
+      </tr>
+   {% endfor %}
+   </table>
+
+   <p>
+   <a href='./show_latest'>show latest results, by architecture</a>
+   <p>
+   <a href='./show_all'>show all results</a>
+{% else %}
+ No results for this package!
+{% endif %}
+"""
+
+        return Template(html).render(locals())
+
+    def show_all(self):
+        package = self.package
+        all_results = self.coord.get_all_results_for_package(package)
+        
+        def calc_status(status):
+            print 'STATUS:', status
+            if status:
+                return "<font color='green'>SUCCESS</font>"
+            else:
+                return "<font color='red'>FAILURE</font>"
+
+        def calc_time(t):
+            return format_timestamp(t)
+
+        def nicetagset(tagset):
+            tagset = sorted([ x for x in tagset if not x.startswith('__')])
+            return ", ".join(tagset)
+
+        html = """
+<title>All build results for `{{ package }}`</title>
+<h2>Package '{{ package }}'</h2>
+All build information:<p>
+{% if all_results %}
+   <table border='1'>
+      <tr><th>Host</th><th>Arch</th><th>Status</th><th>Tags</th><th>Time</th></tr>
+   {% for (receipt, client_info, results_list) in all_results %}
+      <tr>
+        <td>{{ client_info['host'] }}</td>
+        <td>{{ client_info['arch'] }}</td>
+        <td>{{ calc_status(client_info['success']) }}</td>
+        <td>{{ nicetagset(client_info['tags']) }}</td>
+        <td>{{ calc_time(receipt['time']) }}</td>
+      </tr>
+   {% endfor %}
+   </table>
+
+   <p>
+   <a href='./'>show default report</a>
+   <p>
+   <a href='./show_latest'>show latest results, by architecture</a>
 {% else %}
  No results for this package!
 {% endif %}
