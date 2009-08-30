ad1cca842d42ddaa7f29e2a925cc63f53d5d20d6
Titus Brown <t@titus-browns-macbook.local>
2009-08-30 11:14:48 -0400

    .

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='pony-build',
-      version='0.5+20090802',
+      version='0.5+20090829',
       description='pony-build, a simple Continuous Integration framework',
       author = 'C. Titus Brown',
       author_email = 't@idyll.org',
       url = 'http://github.com/ctb/pony-build',
       license = 'BSD',
       packages = ['pony_build'],
+      test_suite = 'nose.collector'
 #      py_modules = ['client/pony_build_client'],
       )
