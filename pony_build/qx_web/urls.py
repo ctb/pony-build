3755df8fb803bdce7f12e81abee5f0e773962325
Titus Brown <t@titus-browns-macbook-2.local>
2009-11-22 01:55:38 -0500

    fix for result URL in rss feeds

--- pony_build/qx_web/urls.py
+++ pony_build/qx_web/urls.py
@@ -3,7 +3,7 @@ base_url = None
 named_rss_feed_url = '/rss2/%(feedname)s'
 generic_rss_feed_root = '/rss2/_generic/%(package)s/'
 package_url_template = 'p/%(package)s/'
-per_result_url_template = 'p/%(package)s/detail?result_key=%(result_key)s'
+per_result_url_template = 'p/%(package)s/%(result_key)s/'
 
 def calculate_base_url(host, port, script_name=''):
     if not host.strip():
