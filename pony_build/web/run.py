a461fc7198f57b858d3838c54edc269b1df80f87
C. Titus Brown <titus@idyll.org>
2010-04-17 18:40:30 -0400

    set default server port to match default client port

--- pony_build/web/run.py
+++ pony_build/web/run.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser.add_option('-i', '--interface', dest='interface',
                       help='interface to bind', default='localhost')
     parser.add_option('-p', '--port', dest='port', help='port to bind',
-                      type='int', default='5000')
+                      type='int', default='8000')
     parser.add_option('-f', '--dbfile', dest='dbfile',
                      help='database filename', type='string',
                       default=':memory:')
